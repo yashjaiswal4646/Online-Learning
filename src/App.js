7d0ec526bfafd581c647a01d0b1de64362ceec3a
Yash Jaiswal <architect4646@gmail.com>
2025-03-02 22:33:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { CourseHero } from './Nodejscourse/CourseHero';
-import { CourseDetails } from './Nodejscourse/CourseDetails';
-import { CourseSyllabus } from './Nodejscourse/CourseSyllabus';
-import { ValueProposition } from './Nodejscourse/ValueProposition';
+// import { Flutter } from './AllCourses/Flutter';
+import Courses from './Courses';
 function App() {
   return (
     
       <main className="flex flex-col pb-16 overflow-hidden bg-stone-950">
       <Navbar/>
       
-      <CourseHero />
-      <CourseDetails />
-      <CourseSyllabus />
+      {/* <Flutter /> */}
+      <Courses />
       
-      <ValueProposition />    
       <Footer />
     </main>
     
