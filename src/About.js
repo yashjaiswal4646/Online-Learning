51e22427c0cf4bd397dca4e30c0144e6508a6baa
Yash <architect4646@gmail.com>
2025-03-07 13:20:21 +0530

    about

--- src/About.js
+++ src/About.js
@@ -51,30 +51,34 @@ const About = ({ image, name, role }) => {
   
 
   
-      <section className="flex flex-col items-center mr-16 mt-36 max-md:mt-10">
-        <h2 className="text-white text-7xl max-md:max-w-full max-md:text-4xl">
+      <section className="flex flex-col items-center mr-28 mt-36 max-md:mt-10">
+        <h2 className="ml-16 text-white text-7xl max-md:max-w-full max-md:text-4xl">
           People who makes us a team
         </h2>
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e3618939632f93eeb65565587e0f3c45e5750ec6154f23ea215ec45ef64018a?placeholderIfAbsent=true&apiKey=6174043aed6e4993a0e569f439eb050d"
           alt="Team"
-          className="object-contain mt-28 w-full aspect-[1.5] max-w-[895px] max-md:mt-10 max-md:max-w-full"
+          className="object-contain mt-28 w-full aspect-[1.5] max-w-[895px] ml-16 max-md:mt-10 max-md:max-w-full"
         />
+
+        
         <div className="self-end mt-32 w-full max-w-[1205px] max-md:mt-10 max-md:max-w-full">
           <div className="flex gap-5 max-md:flex-col">
             <div className="w-6/12 max-md:w-full">
               <div className="flex flex-col items-center w-full text-xl text-white">
-                <h3 className="text-5xl max-md:text-4xl">OUR LEADERS</h3>
-                <div className="flex flex-col self-stretch w-full px-3 mt-5">
+                <h3 className="mt-5 text-5xl max-md:text-4xl">OUR LEADERS</h3>
+                <hr className="shrink-0 self-start mt-5 ml-3 mb-6  max-w-full h-px border border-white border-solid w-[555px]" />
+                <div className="flex flex-col self-stretch w-full px-3 mt-10 gap-28">
                   <LeaderCard
                     image="https://cdn.builder.io/api/v1/image/assets/TEMP/75cd1b7457449251286a559899c133409c580f1b65c7bf642dfdf7fe65212136?placeholderIfAbsent=true&apiKey=6174043aed6e4993a0e569f439eb050d"
                     name="Rizwan Ali"
-                    role="FOUNDER & CEO"
+                    role="FOUNDER"
                   />
                   <LeaderCard
                     image="https://cdn.builder.io/api/v1/image/assets/TEMP/aece956ab47882c8d1443be57b0c60a23efb032f66690be78fcc83617cdeeea5?placeholderIfAbsent=true&apiKey=6174043aed6e4993a0e569f439eb050d"
                     name="Sunil Kumar"
-                    role="CHRO & INSTR."
+                    role="FOUNDER"
+                    
                   />
                 </div>
               </div>
@@ -84,7 +88,7 @@ const About = ({ image, name, role }) => {
                 <LeaderCard
                   image="https://cdn.builder.io/api/v1/image/assets/TEMP/7b059bfe616dc7129964dc09593c695f3d59959c6ecdf3b6d7f698132bdcadd1?placeholderIfAbsent=true&apiKey=6174043aed6e4993a0e569f439eb050d"
                   name="Yash Jaiswal"
-                  role="MANAGING DIRECTOR & INSTR."
+                  role="FOUNDER"
                 />
               </div>
             </div>
@@ -101,7 +105,7 @@ const About = ({ image, name, role }) => {
       </h2>
 
       <div className="flex flex-wrap gap-10 mt-24 w-full text-5xl text-white max-w-[901px] max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-        <h3 className="grow shrink w-[134px] max-md:text-4xl">PHONE</h3>
+        <h3 className="grow shrink w-[134px] max-md:text-4xl">PHONE :</h3>
         <p className="grow shrink w-[562px] max-md:max-w-full max-md:text-4xl">
           9874563212
         </p>
@@ -110,8 +114,8 @@ const About = ({ image, name, role }) => {
         <div className="flex gap-5 max-md:flex-col">
           <div className="w-[26%] max-md:ml-0 max-md:w-full">
             <div className="flex flex-col text-5xl text-white grow whitespace-nowrap max-md:mt-10 max-md:text-4xl">
-              <h3 className="self-start max-md:text-4xl">EMAIL</h3>
-              <h3 className="mt-20 max-md:mt-10 max-md:text-4xl">ADDRESS</h3>
+              <h3 className="self-start max-md:text-4xl">EMAIL :</h3>
+              <h3 className="mt-20 max-md:mt-10 max-md:text-4xl">ADDRESS  :</h3>
             </div>
           </div>
           <div className="ml-5 w-[74%] max-md:ml-0 max-md:w-full">
